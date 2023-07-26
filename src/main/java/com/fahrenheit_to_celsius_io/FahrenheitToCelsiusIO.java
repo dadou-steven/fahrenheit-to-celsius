7d28fe1f8fb895b54c011c5511a2471689f83d72
DADOU STEVEN <dgv1pro@gmail.com>
2023-07-26 23:29:58 +0200

    dadou

--- src/main/java/com/fahrenheit_to_celsius_io/FahrenheitToCelsiusIO.java
+++ src/main/java/com/fahrenheit_to_celsius_io/FahrenheitToCelsiusIO.java
@@ -4,35 +4,12 @@ import java.util.Scanner;
 
 public class FahrenheitToCelsiusIO {
 
-    // déclaration de variables d'instance
-
-    private int fahrenheit;
-
-    // constructeur
-
-    public FahrenheitToCelsiusIO() {
-    }
-
-    // accesseurs (getters) et mutateurs (setters)
-
-    public int getFahrenheit() {
-        return fahrenheit;
-    }
-
-    public void setFahrenheit(int fahrenheit) {
-        this.fahrenheit = fahrenheit;
-    }
-
-    // méthodes
-
-    public void fahrenheitCelsius() {
-        double jours = (fahrenheit - 32) * (5D / 9);
-        System.out.println(jours);
-    }
-
     // afficher le résultat
 
     public static void main(String[] args) {
+
+        final int MIN_TEMP = 60;
+        final int MAX_TEMP = 85;
 
         Scanner input = new Scanner(System.in);
         System.out.print("Entrer une valeur Fahrenheit : ");
@@ -40,14 +17,24 @@ public class FahrenheitToCelsiusIO {
         System.out.print("Entrer le jour concerné : ");
         String days = input.next();
         double celsius = (fahrenheit - 32) * (5D / 9);
+
+        boolean raining = false;
+        System.out.print("Les averses ? (y/n) : ");
+        String rainInput = input.next().toLowerCase();
+        if (rainInput.startsWith("y")) {
+            raining = true;
+        }
+
         System.out.printf("%s %s %.1f \n", days + " En Fahrenheit : ", fahrenheit + "° -> En Celsius : ", celsius);
 
-        if ((fahrenheit >= 60) && (fahrenheit <= 85)) {
+        if ((fahrenheit >= 60) && (fahrenheit <= 85) && (!raining)) {
             System.out.println("Vive le beau temps !");
         } else if (fahrenheit > 85) {
             System.out.println("Reste à la maison, il fait très chaud !");
-        } else {
+        } else if (fahrenheit < 60) {
             System.out.println("Reste à la maison, il fait froid !");
+        } else {
+            System.out.println("Ne sorte pas, il pleut !");
         }
     }
 }
